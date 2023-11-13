78651ca5fd793ca847447c63ecae17c659f0d86b
MelicherikM <matusmelicherik1@gmail.com>
2023-11-13 11:16:49 +0100

    PSA cv08, cdp: tlv_device_id, tlv_port_id, tlv_software class  + updates and fixes

--- T7, T8/cdp.py
+++ T7, T8/cdp.py
@@ -2,12 +2,14 @@
 from scapy.all import *
 import struct
 
+
 def mac_to_bytes(mac):
     mac = mac.replace(":", "")
     mac_bytes = bytes.fromhex(mac)
     return mac_bytes
 
-class Eth_hdr():
+
+class Eth_hdr:
     def __init__(self, src_mac):
         self.dst_mac = "01:00:0c:cc:cc:cc"
         self.src_mac = src_mac
@@ -17,13 +19,15 @@ class Eth_hdr():
     def to_bytes(self):
         if self.payload == None:
             return None
-        return mac_to_bytes(self.dst_mac) + mac_to_bytes(self.src_mac) + struct.pack("!H", self.length) + self.payload.to_bytes()
+        return mac_to_bytes(self.dst_mac) + mac_to_bytes(self.src_mac) + struct.pack("!H",
+                                                                                     self.length) + self.payload.to_bytes()
 
     def add_payload(self, payload):
         self.payload = payload
         self.length = len(payload.to_bytes())
 
-class Llc_hdr():
+
+class Llc_hdr:
     def __init__(self):
         self.dsap = 0xaa
         self.ssap = 0xaa
@@ -36,13 +40,15 @@ class Llc_hdr():
         self.payload = payload
 
     def to_bytes(self):
-        return struct.pack("!3B", self.dsap, self.ssap, self.ctrl) + mac_to_bytes(self.oui) + struct.pack("!H", self.pid) + self.payload.to_bytes()
+        return struct.pack("!3B", self.dsap, self.ssap, self.ctrl) + mac_to_bytes(self.oui) + struct.pack("!H",
+                                                                                                          self.pid) + self.payload.to_bytes()
 
-class CDP_hdr():
+
+class CDP_hdr:
     def __init__(self):
         self.version = 1
         self.ttl = 180
-        self.checksum = 0
+        self.checksum = 0xbf42
         self.payload = list()
 
     def add_payload(self, payload):
@@ -54,7 +60,8 @@ class CDP_hdr():
             payload_bytes += payload.to_bytes()
         return struct.pack("!BBH", self.version, self.ttl, self.checksum) + payload_bytes
 
-class TLV():
+
+class TLV:
     def __init__(self, type):
         self.type = type
         self.length = 4
@@ -62,17 +69,50 @@ class TLV():
     def to_bytes(self):
         return struct.pack("!HH", self.type, self.length)
 
+
 class TLV_device_id(TLV):
     def __init__(self, hostname):
         TLV.__init__(self, 0x0001)
         self.value = hostname
+        value_bytes = self.value.encode()
+        self.length += len(value_bytes)
 
     def to_bytes(self):
         value_bytes = self.value.encode()
-        self.length += len(value_bytes)
+        print(self.length)
         return TLV.to_bytes(self) + value_bytes
 
-#IFACES.show()
+
+class TLV_port_id(TLV_device_id):
+    def __init__(self, port):
+        super().__init__(port)
+        self.type = 0x0003
+
+
+class TLV_software(TLV_device_id):
+    def __init__(self):
+        super().__init__("Windows 11 23H2")
+        self.type = 0x0005
+
+class TLV_capabilities(TLV):
+    def __init__(self, router=False, switch=False, host=True):
+        TLV.__init__(self, 0x0004)
+        self.capabilities = 0
+        self.length = 8 # constantna cela dlzka TLV
+        if router:
+            self.capabilities = self.bit_set(self.capabilities, 1)
+        if switch:
+            self.capabilities = self.bit_set(self.capabilities, 4)
+        if host:
+            self.capabilities = self.bit_set(self.capabilities, 5)
+
+    def bit_set(self, vstup, por_bit):
+        return vstup | (1 << por_bit-1)
+
+    def to_bytes(self):
+        return TLV.to_bytes(self) + struct.pack("!I", self.capabilities)
+
+# IFACES.show()
 
 # index 18 MediaTek Wi-Fi 6 MT7921 Wireless LAN Card
 interface = IFACES.dev_from_index(18)
@@ -81,8 +121,14 @@ sock = conf.L2socket(iface=interface)
 eth_hdr = Eth_hdr("00:11:22:33:44:55")
 llc_hdr = Llc_hdr()
 cdp_hdr = CDP_hdr()
-tlv_device_id = TLV_device_id("IdeaPadWin11")
+tlv_device_id = TLV_device_id("IdeaPad")
 cdp_hdr.add_payload(tlv_device_id)
+tlv_port_id = TLV_software("wifi")
+cdp_hdr.add_payload(tlv_port_id)
+tlv_cap = TLV_capabilities(host=True, router=True, switch=True)
+cdp_hdr.add_payload(tlv_cap)
+tlv_software = TLV_software()
+cdp_hdr.add_payload(tlv_software)
 llc_hdr.add_payload(cdp_hdr)
 eth_hdr.add_payload(llc_hdr)
 sock.send(eth_hdr.to_bytes())
