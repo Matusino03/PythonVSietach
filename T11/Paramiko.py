7a47209807624523d97a87c0c7518b50f089202d
matus <matusmelicherik1@gmail.com>
2023-12-04 11:33:26 +0100

    paramiko-microtik

--- T11/Paramiko.py
+++ T11/Paramiko.py
@@ -10,9 +10,15 @@ ssh_client.set_missing_key_policy(AutoAddPolicy())
 ssh_client.connest(hostname=IP, port=PORT, username=USER, password=PASS)
 (stdin, stdout, stderr) = ssh_client.exec_command("sh ip int br")
 vystup = list()
+counter 0
 for line in stdout:
+    conter += 1
+    if counter < 3:
+        continue
     riadok_list = line.strip("\n")..strip("\r").split("\t")
     while "" in riadok_list:
         riadik_list.remove("")
-    print(riadok_list)
+        vystup.append({"interface": riadok_list[0], "ip": riadok_list[1]})
+    
+    print(vystup)
 
