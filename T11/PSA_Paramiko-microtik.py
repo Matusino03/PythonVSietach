38e398b141a36428e4579001b197a9d2b54ee98a
Matusino03 <matusmelicherik1@gmail.com>
2024-01-11 15:14:24 +0100

    fix

--- T11/PSA_Paramiko-microtik.py
+++ T11/PSA_Paramiko-microtik.py
@@ -1,26 +1,21 @@
-from paramiko import *
+import paramiko
+from paramiko.client import AutoAddPolicy
 
 IP = "158.193.152.167"
 PORT = 22
 USER = "admin"
 PASS = "Admin123"
 
-ssh_client = SSHCLIENT
-ssh_client.set_missing_key_policy(AutoAddPolicy())
-ssh_client.connest(hostname=IP, port=PORT, username=USER, password=PASS, look_for_key)
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+ssh_client.connect(hostname=IP, port=PORT, username=USER, password=PASS, look_for_keys=False)
 
 (stdin, stdout, stderr) = ssh_client.exec_command("/ip address print")
-vystup = list()
-counter 0
+vystup = []
+counter = 0
 for line in stdout:
-    line_list = line.strip("\n").strip("\r").split(" ")
+    line_list = line.strip().split(" ")
     if len(line_list) < 2:
         continue
     vystup.append({"interface": line_list[3].split("=")[1], "ip": line_list[1].split("=")[1]})
-    print(vystup)
-
-#(stdin, stdout, stderr) = ssh_client.exec_command("conf t")
-#(stdin, stdout, stderr) = ssh_client.exec_command("int lo0")
-#(stdin, stdout, stderr) = ssh_client.exec_command("ip add 1.1.1.1 255.255.255.255")
-
-
+print(vystup)
